76dd2309c7cf605ad54d3fae470ab0ea29a846db
Lloyd Markle <lloyd.markle@gmail.com>
2026-01-07 14:03:14 -0800

    fix github deployment...

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/senalumi/',
   server: {
-    cors: true,
     host: '0.0.0.0',
-    proxy: {
-      '/server': {
-        target: 'http://localhost:2567',
-        ws: true,
-      },
-    },
   },
 })
